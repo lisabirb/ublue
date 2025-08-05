128ebf617d229156ccb13ee9a03c0b13890da735
lisabirb <93887296+lisabirb@users.noreply.github.com>
2025-08-05 03:23:17 -0600

    langpacks are already included with the ublue base image, so don't install them

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,6 @@ set -ouex pipefail
 dnf5 -y install dnf-plugins-core flatpak
 dnf4 -y copr enable yalter/niri
 dnf5 -y group install base-x
-dnf5 -y install glibc-langpack-en
 dnf5 -y install niri
 dnf5 -y install greetd gtkgreet
 dnf5 -y install cage labwc
