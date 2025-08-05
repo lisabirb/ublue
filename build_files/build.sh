629b2772338742edd7c02f80f2b0cd9f90f0b1ca
lisabirb <93887296+lisabirb@users.noreply.github.com>
2025-08-04 18:18:13 -0600

    add dnf-plugins-core

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,9 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
+dnf5 -y install dnf-plugins-core flatpak
 dnf5 -y copr enable yalter/niri
 dnf5 -y install niri
-dnf5 -y install flatpak fastfetch alacritty fish
+dnf5 -y install fastfetch alacritty fish
 
 # Use a COPR Example:
 #
